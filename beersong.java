c69417e4fcb9375fd4abf6093d48109645c4c555
Emily Chai <echai93@gmail.com>
2017-12-24 10:33:12 +0000

    Correct errors - println and

--- beersong.java
+++ beersong.java
@@ -3,56 +3,56 @@
 public class BeerSong {
 	public static void main (String[] args) {
 		int beerNum = 99;
-		String word = “bottles”;
+		String word = "bottles";
 
 		while (beerNum > 0) {
 
 			if (beerNum == 1) {
-				word = “bottle”;
+				word = "bottle";
 			}
 
-			System.out.printIn(beerNum + “ “ + word + “ of beer on the wall”);
-			System.out.printIn(beerNum + “ “ + word + “ of beer.”);
-			System.out.printIn(“Take one down.”);
-			System.out.printIn(“Pass it around”);
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around");
 			beerNum = beerNum - 1;
 
 			if (beerNum > 0) {
-				System.out.printIn(beerNum + “ “ + word + “ of beer on the wall”);
+				System.out.println(beerNum + " " + word + " of beer on the wall");
 			} else {
-				System.out.printIn(“No more bottles of beer on the wall”);
+				System.out.println("No more bottles of beer on the wall");
 			}
 		}
 	}
 }
 
 // Correction
-// 
-// public class BeerSong {
-// 	public static void main (String[] args) {
-// 		int beerNum = 99;
-// 		String word = “bottles”;
-//
-// 		while (beerNum > 0) {
-//
-// 			System.out.printIn(beerNum + “ “ + word + “ of beer on the wall”);
-// 			System.out.printIn(beerNum + “ “ + word + “ of beer.”);
-// 			System.out.printIn(“Take one down.”);
-// 			System.out.printIn(“Pass it around”);
-//
-// 			beerNum = beerNum - 1;
-// 			if (beerNum == 1) {
-// 				word = “bottle”;
-// 			}
-//
-// 			if (beerNum > 0) {
-// 				System.out.printIn(beerNum + “ “ + word + “ of beer on the wall”);
-// 			} else {
-// 				System.out.printIn(“No more bottles of beer on the wall”);
-// 			}
-// 		}
-// 	}
-// }
+
+public class BeerSong {
+	public static void main (String[] args) {
+		int beerNum = 99;
+		String word = "bottles";
+
+		while (beerNum > 0) {
+
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around");
+
+			beerNum = beerNum - 1;
+			if (beerNum == 1) {
+				word = "bottle";
+			}
+
+			if (beerNum > 0) {
+				System.out.println(beerNum + " " + word + " of beer on the wall");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			}
+		}
+	}
+}
 
 
 // Also the same
@@ -60,24 +60,24 @@ public class BeerSong {
 // public class BeerSong {
 // 	public static void main (String[] args) {
 // 		int beerNum = 99;
-// 		String word = “bottles”;
+// 		String word = "bottles";
 //
 // 		while (beerNum > 0) {
 //
-// 			System.out.printIn(beerNum + “ “ + word + “ of beer on the wall, ” + beerNum + “ “ + word + “ of beer”);
+// 			System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer");
 // 			beerNum = beerNum - 1;
 //
 // 			if (beerNum == 1) {
-// 				word = “bottle”;
+// 				word = "bottle";
 // 			}
 //
 // 			if (beerNum > 0) {
-// 				System.out.printIn(“Take one down, pass it round “ + beerNum + “ “ + word + “ of beer”);
+// 				System.out.println("Take one down, pass it round " + beerNum + " " + word + " of beer");
 // 			}
 // 		}
 //
 // 		if (beerNum == 0) {
-// 			System.out.printIn(“No more bolts of beer”);
+// 			System.out.println("No more bolts of beer");
 //
 // 		}
 // 	}
