de3eea8490aa4c70fe8e4eee8dea5c8a8153f9f3
Emily Chai <echai93@gmail.com>
2017-12-25 09:34:54 +0000

    Dog barks differ for the size of the dog

--- ChapterFour/Dog.java
+++ ChapterFour/Dog.java
@@ -3,9 +3,9 @@ class Dog {
   String name;
 
   void bark() {
-    if (size < 60) {
+    if (size > 60) {
       System.out.println("Woof! Wooof");
-    } else if (size < 14) {
+    } else if (size > 14) {
       System.out.println("Ruff! Ruff!");
     } else {
       System.out.println("Yip! Yip!");
