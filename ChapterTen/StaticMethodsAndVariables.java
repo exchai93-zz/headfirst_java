f5375476dcccff46bb45a0fee940156917edc875
Emily Chai <echai93@gmail.com>
2017-12-28 17:53:55 +0000

    Static variables give a value shared by all instances

--- ChapterTen/StaticMethodsAndVariables.java
+++ ChapterTen/StaticMethodsAndVariables.java
@@ -14,4 +14,22 @@ public class Song {
 public static int min(int a, int b) {
   // returns the lesser of a and b
 }
-// min() comes from the Math class which does not require an instance variable 
+// min() comes from the Math class which does not require an instance variable
+
+// Static variable
+public class Duck {
+  private int size;
+  // The static duckCount variable is initialized ONLY when the class is first lodaded, NOT each time a new instance is made
+  private static int duckCount = 0;
+
+  public Duck() {
+    duckCount++;
+  }
+
+  public void setSize(int s) {
+    size = s;
+  }
+  public int getSize() {
+    return size;
+  }
+}
