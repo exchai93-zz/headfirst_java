ddf961c57bf6993d2d7901e1ac2e03ae7b5efdec
Emily Chai <echai93@gmail.com>
2017-12-28 11:47:54 +0000

    Using superclass constructors with args

--- ChapterNine/SuperclassConstructor.java
+++ ChapterNine/SuperclassConstructor.java
@@ -7,3 +7,36 @@ public class Duck extends Animal {
     size = newSize;
   }
 }
+
+
+// Superclass constructors with arguments
+public abstract class Animal {
+  // All animals have a name
+  private String name;
+  }
+  // A getter method that Hippo inherits
+  public String getName() {
+    return name;
+  }
+  // The constructor that takes the name and assigns it the name instance variable
+  public Animal(String theName) {
+    name = theName;
+  }
+}
+
+public class Hippo extends Animal {
+  // Hippo constructor takes a name
+  public Hippo(String name) {
+    // Sends the name up the Stack to the Animal constructor
+    super(name);
+  }
+}
+
+public class MakeHippo {
+  public static void main(String [] args) {
+    // Make a Hippo passing the name "Buffy" to the Hippo constructor
+    Hippo h = new Hippo("Buffy");
+    // Call the Hippo's inherited getName()
+    System.out.println(h.getName());
+  }
+}
