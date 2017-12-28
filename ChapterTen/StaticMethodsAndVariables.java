2e851b9ce85d4fc23f9f6ac518839fa46b5127ce
Emily Chai <echai93@gmail.com>
2017-12-28 17:51:01 +0000

    Static and non static methods

---
+++ ChapterTen/StaticMethodsAndVariables.java
@@ -0,0 +1,17 @@
+// Regular method
+public class Song {
+  String title;
+  public Song(String t) {
+    title = t;
+  }
+  public void play() {
+    SoundPlayer = new SoundPlayer();
+    player.playSound(Title);
+  }
+}
+
+// Static method
+public static int min(int a, int b) {
+  // returns the lesser of a and b
+}
+// min() comes from the Math class which does not require an instance variable 
