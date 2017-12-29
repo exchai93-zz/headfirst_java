6eaf125bfb011cf3b7ecf77798daf53512e3b645
Emily Chai <echai93@gmail.com>
2017-12-29 16:14:44 +0000

    Wrapping with and without autoboxing

--- ChapterTen/WrappingPrimitives.java
+++ ChapterTen/WrappingPrimitives.java
@@ -16,10 +16,11 @@ public void doNumsOldWay() {
   int intOne = one.intValue();
 }
 
-// With autoboxing - Java 5.0 onwards 
+// With autoboxing - Java 5.0 onwards
 public void doNumsNewWay() {
   // Make an ArrayList of type Integer
   ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
   listOfNumbers.add(3);
+  // Compiler automatically unwraps the Integer object 
   int num = listOfNumbers.get(0);
 }
