f56aff78f67f36f50a5345fecd51aa2bb8a8dc5d
Emily Chai <echai93@gmail.com>
2017-12-29 09:10:19 +0000

    Using assertions for debugging

---
+++ AppendixB/Assertions.java
@@ -0,0 +1,4 @@
+// Add assertions to your code wherever you believe that something must be true
+assert (height > 0);
+// if true, the program continues normally
+// f false, throw an AssertionError 
