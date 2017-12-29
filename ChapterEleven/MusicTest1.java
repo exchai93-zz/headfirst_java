ca47d2ee41b8a11b08cad3f8cb88380ca402e344
Emily Chai <echai93@gmail.com>
2017-12-29 15:27:58 +0000

    Wrap risky code in a try/catch

--- ChapterEleven/MusicTest1.java
+++ ChapterEleven/MusicTest1.java
@@ -2,8 +2,13 @@ import javax.sound.midi.*;
 
 public class MusicTest1 {
   public void play() {
-    Sequencer sequencer = MidiSystem.getSequencer();
-    System.out.println("We got a sequencer");
+
+    try {
+      Sequencer sequencer = MidiSystem.getSequencer();
+      System.out.println("We got a sequencer");
+    } catch(MidiUnavailableException ex) {
+      System.out.println("Bummer");
+    }
   }
 
   public static void main(String [] args) {
