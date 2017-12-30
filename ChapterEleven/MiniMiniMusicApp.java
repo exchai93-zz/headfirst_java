0037a79554fee20012542f2ae41f6c0dcf8dc8aa
Emily Chai <echai93@gmail.com>
2017-12-30 10:12:55 +0000

    Create first sound player app using midi package

---
+++ ChapterEleven/MiniMiniMusicApp.java
@@ -0,0 +1,32 @@
+import javax,sound.midi.*;
+
+public class MiniMiniMusicApp {
+  MiniMiniMusicApp mini = new MiniMiniMusicApp();
+  mini.play();
+}
+
+public void play() {
+  try {
+    Sequencer player = MidiSystem.getSequencer();
+    player.open();
+
+    Sequencers seq = new Sequencer(Sequence.PPQ, 4);
+    Track track = seq.createTrack();
+
+      ShortMessage a = new ShortMessage();
+      a.setMessage(144, 1, 44, 100);
+      MidiEvent noteOn = new MidiEvent(a, 1);
+      trackadd(noteOn);
+
+      ShortMessage b = new ShortMessage();
+      b.setMessage(128, 1, 44, 100);
+      MidiEvent noteOff = new MidiEvent(b, 16);
+      track.add(noteOff);
+
+      player.setSequence(seq);
+      player.start();
+    } catch(Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
